9d9ac5cb10291b3257b5c692e1d3508536f2e8c0
ZaurO5 <165468417+ZaurO5@users.noreply.github.com>
2024-06-08 11:30:58 +0400

    Abs.

---
+++ Abstractions/IReadRepo.cs
@@ -0,0 +1,15 @@
+﻿using Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractions
+{
+    public interface IReadRepo<T> where T : BaseModel
+    {
+        List<T> GetAll();
+        T? GetById(int id);
+    }
+}
---
+++ Abstractions/IWriteRepo.cs
@@ -0,0 +1,16 @@
+﻿using Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractions
+{
+    public interface IWriteRepo<T> where T : BaseModel
+    {
+        void Create(T model);
+        void Delete(int id);
+        void Update(int id, T model);
+    }
+}
